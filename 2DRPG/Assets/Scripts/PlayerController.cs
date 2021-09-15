66f23a763f772216afbafb4df4cca4a20718e5ff
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2021-09-15 17:36:06 +0900

    Unity

--- 2DRPG/Assets/Scripts/PlayerController.cs
+++ 2DRPG/Assets/Scripts/PlayerController.cs
@@ -13,8 +13,21 @@ public class PlayerController : MonoBehaviour
     public GameManager gameManager;
 
     Animator anim;
-
     Rigidbody2D rigid;
+
+    //Mobile Key Value
+    int up_Value;
+    int down_Value;
+    int right_Value;
+    int left_Value;
+    bool up_Down;
+    bool down_Down;
+    bool left_Down;
+    bool right_Down;
+    bool up_Up;
+    bool down_Up;
+    bool right_Up;
+    bool left_Up;
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
@@ -24,14 +37,16 @@ public class PlayerController : MonoBehaviour
      void Update()
      {
         //Move Value
-         h = gameManager.isAction ? 0 : Input.GetAxisRaw("Horizontal");
-         v = gameManager.isAction ? 0 : Input.GetAxisRaw("Vertical");
+        //PC
+         h = gameManager.isAction ? 0 : Input.GetAxisRaw("Horizontal")+ right_Value + left_Value;
+        v = gameManager.isAction ? 0 : Input.GetAxisRaw("Vertical")+ up_Value + down_Value;
+   
 
         //Check Button Down & Up
-        bool hDown = gameManager.isAction ? false : Input.GetButtonDown("Horizontal"); 
-        bool vDown = gameManager.isAction ? false : Input.GetButtonDown("Vertical");
-        bool hUp = gameManager.isAction ? false : Input.GetButtonUp("Horizontal");
-        bool vUp = gameManager.isAction ? false : Input.GetButtonUp("Vertical");
+        bool hDown = gameManager.isAction ? false : Input.GetButtonDown("Horizontal") || right_Down || left_Down; 
+        bool vDown = gameManager.isAction ? false : Input.GetButtonDown("Vertical") || up_Down || down_Down;
+        bool hUp = gameManager.isAction ? false : Input.GetButtonUp("Horizontal")|| right_Up || left_Up;
+        bool vUp = gameManager.isAction ? false : Input.GetButtonUp("Vertical") || up_Up || down_Up;
 
         //check Horzontal Move
         if (hDown)
@@ -71,7 +86,17 @@ public class PlayerController : MonoBehaviour
         if (Input.GetButtonDown("Jump") && scanObject != null)
             gameManager.Action(scanObject);
 
-     }
+        //Movile Var Init
+        up_Down = false;
+        down_Down = false;
+        left_Down = false;
+        right_Down = false;
+        up_Up = false;
+        down_Up = false;
+        right_Up = false;
+        left_Up = false;
+
+    }
         
         
     void FixedUpdate()
@@ -93,6 +118,66 @@ public class PlayerController : MonoBehaviour
             scanObject = null;
     }
 
+    public void ButtonDown(string type)
+    {
+        switch (type)
+        {
+            case "U":
+                up_Value = 1;
+                up_Down = true;
+                break;
+            case "D":
+                down_Value = -1;
+                down_Down = true;
+
+                break;
+            case "R":
+                right_Value = 1;
+                right_Down = true;
+
+                break;
+            case "L":
+                left_Value = -1;
+                left_Down = true;
+
+                break;
+
+            case "A":
+                //Scan Object
+                if (scanObject != null)
+                    gameManager.Action(scanObject);
+                break;
+
+            case "C":
+                gameManager.SubMenuActive();
+                break;
+        }
+    }
+
+    public void ButtonUp(string type)
+    {
+        switch (type)
+        {
+            case "U":
+                up_Value = 0;
+                up_Up = true;
+                break;
+            case "D":
+                down_Value = 0; 
+                down_Up = true;
+
+                break;
+            case "R":
+                right_Value = 0;
+                right_Up = true;
+
+                break;
+            case "L":
+                left_Value = 0;
+                left_Up = true;
+                break;
+        }
+    }
     
-   
+
 }
---
+++ 2DRPG/Library/Il2cppBuildCache/Android/armeabi-v7a/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,207 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void GameManager::Start()
+extern void GameManager_Start_m26461AEF27E44DB8FECCBC19D6C9E228B658BF8E (void);
+// 0x00000002 System.Void GameManager::Awake()
+extern void GameManager_Awake_m22F42B2A82708B10F652CAD8F2E0A4767110FF30 (void);
+// 0x00000003 System.Void GameManager::Update()
+extern void GameManager_Update_mC9303BA7C3117BD861F49F8E36151CC52117E6C1 (void);
+// 0x00000004 System.Void GameManager::SubMenuActive()
+extern void GameManager_SubMenuActive_m5594B250B8494F9C4CD2D24BB37DC3698A5D553E (void);
+// 0x00000005 System.Void GameManager::Action(UnityEngine.GameObject)
+extern void GameManager_Action_m534E461B332BA8A5E91C21117C3BD156C2ED5CDB (void);
+// 0x00000006 System.Void GameManager::Talk(System.Int32,System.Boolean)
+extern void GameManager_Talk_m675B5BCF3F39EC57DC6F4F9D45E6575D95684217 (void);
+// 0x00000007 System.Void GameManager::GameSave()
+extern void GameManager_GameSave_m023A725C090C3981FFDDF00BC3A9799051ACA629 (void);
+// 0x00000008 System.Void GameManager::GameLoad()
+extern void GameManager_GameLoad_m62BDF8C3DD57DEAC38A2843F95784818F68B02B9 (void);
+// 0x00000009 System.Void GameManager::GameExit()
+extern void GameManager_GameExit_m10BDD1B3BE53C36A4CD1FFC620AE233B50C8AB53 (void);
+// 0x0000000A System.Void GameManager::.ctor()
+extern void GameManager__ctor_mE8666F6D0CA9C31E16B719F79780DC4B0245B64D (void);
+// 0x0000000B System.Void ObjData::.ctor()
+extern void ObjData__ctor_m6C21E504C5DBD5DBF519634EC95D864151B85219 (void);
+// 0x0000000C System.Void PlayerController::Awake()
+extern void PlayerController_Awake_m23059564DCFDD324EA9DB966473251896647CD23 (void);
+// 0x0000000D System.Void PlayerController::Update()
+extern void PlayerController_Update_mB31159CAD7DD2329859472554BC9154A83D8E794 (void);
+// 0x0000000E System.Void PlayerController::FixedUpdate()
+extern void PlayerController_FixedUpdate_m54EE3ADAA7597303B1F69849B233D1A68D880B14 (void);
+// 0x0000000F System.Void PlayerController::ButtonDown(System.String)
+extern void PlayerController_ButtonDown_mF0E5EEA9C7341D92E744251591BE247DD42B672F (void);
+// 0x00000010 System.Void PlayerController::ButtonUp(System.String)
+extern void PlayerController_ButtonUp_m2DA4BABCCFF424F2D0B299A4B47DFAA6027FD065 (void);
+// 0x00000011 System.Void PlayerController::.ctor()
+extern void PlayerController__ctor_mF30385729DAFDFCB895C4939F6051DCE6C0327FB (void);
+// 0x00000012 System.Void QuestData::.ctor(System.String,System.Int32[])
+extern void QuestData__ctor_mE538EF663EF1C0935A1B787D81A856D8341E1A97 (void);
+// 0x00000013 System.Void QuestManager::Awake()
+extern void QuestManager_Awake_mF6C43464E7A765F8FAF68DFDB0C66AFD141FE209 (void);
+// 0x00000014 System.Void QuestManager::GenerateData()
+extern void QuestManager_GenerateData_m6963530F80E0F13F24DD002C61B4F731F373CDC3 (void);
+// 0x00000015 System.Int32 QuestManager::GetQuestTalkIndex(System.Int32)
+extern void QuestManager_GetQuestTalkIndex_mFBD56AE85DC6B1F96478AB4019BCF73B5323A07D (void);
+// 0x00000016 System.String QuestManager::CheckQuest(System.Int32)
+extern void QuestManager_CheckQuest_m98B81C1888B5B585D65FD0A960BCF32A936E87E6 (void);
+// 0x00000017 System.String QuestManager::CheckQuest()
+extern void QuestManager_CheckQuest_mE5A3FF5C87424F715B368E47B51EA22A378B5CF2 (void);
+// 0x00000018 System.Void QuestManager::NextQuest()
+extern void QuestManager_NextQuest_mAE230FEEC87946C8AF54CA78E50066A67CC3F919 (void);
+// 0x00000019 System.Void QuestManager::ControllObject()
+extern void QuestManager_ControllObject_mFD5E3C97363C800D0CA1A1CDCBD2597D714D1BB6 (void);
+// 0x0000001A System.Void QuestManager::.ctor()
+extern void QuestManager__ctor_m1AC816ADC9C82128299E151F809CE26026157CE7 (void);
+// 0x0000001B System.Void TalkManager::Awake()
+extern void TalkManager_Awake_mF139ED891D65EBA36743F3CFBA021E28758CB2E2 (void);
+// 0x0000001C System.Void TalkManager::GeneratorData()
+extern void TalkManager_GeneratorData_mF1922C6F1F314D545B20875D86199D4F2CBA7D87 (void);
+// 0x0000001D System.String TalkManager::GetTalk(System.Int32,System.Int32)
+extern void TalkManager_GetTalk_mB8103F7173BF61AF7B5B25CA3A41063367D9BFBA (void);
+// 0x0000001E UnityEngine.Sprite TalkManager::GetPortrait(System.Int32,System.Int32)
+extern void TalkManager_GetPortrait_m91649E396C2E610EC9D79BA1603F6D293A02E05B (void);
+// 0x0000001F System.Void TalkManager::.ctor()
+extern void TalkManager__ctor_mE5A0BA1CDE428119DEB23732A0C7D001C8AD4594 (void);
+// 0x00000020 System.Void TypeEffect::Awake()
+extern void TypeEffect_Awake_mD81D2C8A9554D2D737D7D57589BD6AFFEE1F5D60 (void);
+// 0x00000021 System.Void TypeEffect::setMsg(System.String)
+extern void TypeEffect_setMsg_m995884E73F813846AE2049D6DED75CC6AAB7A298 (void);
+// 0x00000022 System.Void TypeEffect::EffectStart()
+extern void TypeEffect_EffectStart_mAB4F3CC1732D9951EC30D3D3C84387C785ADE881 (void);
+// 0x00000023 System.Void TypeEffect::Effecting()
+extern void TypeEffect_Effecting_m0E83F1D00A1BFC04B5A2E043DB779F304ADCB4F9 (void);
+// 0x00000024 System.Void TypeEffect::EffectEnd()
+extern void TypeEffect_EffectEnd_mD82EA7577352793CDA69C508B4F4FED0B39F3BB0 (void);
+// 0x00000025 System.Void TypeEffect::.ctor()
+extern void TypeEffect__ctor_m3C10101B8FB7EF93083B4F01E732FF79C733A078 (void);
+// 0x00000026 System.Void ReadmeBE2::.ctor()
+extern void ReadmeBE2__ctor_mEC00C782CF4C0DC40BD62EE5873DB3A8D7ADB4DE (void);
+// 0x00000027 System.Void ReadmeBE2/Section::.ctor()
+extern void Section__ctor_m61FFAD941CD821F4F7A88DC539F890AABF3653D1 (void);
+// 0x00000028 System.Void ReadmeBE3::.ctor()
+extern void ReadmeBE3__ctor_m7702AAD83D17D1CA3B1BA28D4F1CA4BD4A19CB8A (void);
+// 0x00000029 System.Void ReadmeBE3/Section::.ctor()
+extern void Section__ctor_mD71981204AE41DE505AA4CB01AD9E433C95A1A99 (void);
+static Il2CppMethodPointer s_methodPointers[41] = 
+{
+	GameManager_Start_m26461AEF27E44DB8FECCBC19D6C9E228B658BF8E,
+	GameManager_Awake_m22F42B2A82708B10F652CAD8F2E0A4767110FF30,
+	GameManager_Update_mC9303BA7C3117BD861F49F8E36151CC52117E6C1,
+	GameManager_SubMenuActive_m5594B250B8494F9C4CD2D24BB37DC3698A5D553E,
+	GameManager_Action_m534E461B332BA8A5E91C21117C3BD156C2ED5CDB,
+	GameManager_Talk_m675B5BCF3F39EC57DC6F4F9D45E6575D95684217,
+	GameManager_GameSave_m023A725C090C3981FFDDF00BC3A9799051ACA629,
+	GameManager_GameLoad_m62BDF8C3DD57DEAC38A2843F95784818F68B02B9,
+	GameManager_GameExit_m10BDD1B3BE53C36A4CD1FFC620AE233B50C8AB53,
+	GameManager__ctor_mE8666F6D0CA9C31E16B719F79780DC4B0245B64D,
+	ObjData__ctor_m6C21E504C5DBD5DBF519634EC95D864151B85219,
+	PlayerController_Awake_m23059564DCFDD324EA9DB966473251896647CD23,
+	PlayerController_Update_mB31159CAD7DD2329859472554BC9154A83D8E794,
+	PlayerController_FixedUpdate_m54EE3ADAA7597303B1F69849B233D1A68D880B14,
+	PlayerController_ButtonDown_mF0E5EEA9C7341D92E744251591BE247DD42B672F,
+	PlayerController_ButtonUp_m2DA4BABCCFF424F2D0B299A4B47DFAA6027FD065,
+	PlayerController__ctor_mF30385729DAFDFCB895C4939F6051DCE6C0327FB,
+	QuestData__ctor_mE538EF663EF1C0935A1B787D81A856D8341E1A97,
+	QuestManager_Awake_mF6C43464E7A765F8FAF68DFDB0C66AFD141FE209,
+	QuestManager_GenerateData_m6963530F80E0F13F24DD002C61B4F731F373CDC3,
+	QuestManager_GetQuestTalkIndex_mFBD56AE85DC6B1F96478AB4019BCF73B5323A07D,
+	QuestManager_CheckQuest_m98B81C1888B5B585D65FD0A960BCF32A936E87E6,
+	QuestManager_CheckQuest_mE5A3FF5C87424F715B368E47B51EA22A378B5CF2,
+	QuestManager_NextQuest_mAE230FEEC87946C8AF54CA78E50066A67CC3F919,
+	QuestManager_ControllObject_mFD5E3C97363C800D0CA1A1CDCBD2597D714D1BB6,
+	QuestManager__ctor_m1AC816ADC9C82128299E151F809CE26026157CE7,
+	TalkManager_Awake_mF139ED891D65EBA36743F3CFBA021E28758CB2E2,
+	TalkManager_GeneratorData_mF1922C6F1F314D545B20875D86199D4F2CBA7D87,
+	TalkManager_GetTalk_mB8103F7173BF61AF7B5B25CA3A41063367D9BFBA,
+	TalkManager_GetPortrait_m91649E396C2E610EC9D79BA1603F6D293A02E05B,
+	TalkManager__ctor_mE5A0BA1CDE428119DEB23732A0C7D001C8AD4594,
+	TypeEffect_Awake_mD81D2C8A9554D2D737D7D57589BD6AFFEE1F5D60,
+	TypeEffect_setMsg_m995884E73F813846AE2049D6DED75CC6AAB7A298,
+	TypeEffect_EffectStart_mAB4F3CC1732D9951EC30D3D3C84387C785ADE881,
+	TypeEffect_Effecting_m0E83F1D00A1BFC04B5A2E043DB779F304ADCB4F9,
+	TypeEffect_EffectEnd_mD82EA7577352793CDA69C508B4F4FED0B39F3BB0,
+	TypeEffect__ctor_m3C10101B8FB7EF93083B4F01E732FF79C733A078,
+	ReadmeBE2__ctor_mEC00C782CF4C0DC40BD62EE5873DB3A8D7ADB4DE,
+	Section__ctor_m61FFAD941CD821F4F7A88DC539F890AABF3653D1,
+	ReadmeBE3__ctor_m7702AAD83D17D1CA3B1BA28D4F1CA4BD4A19CB8A,
+	Section__ctor_mD71981204AE41DE505AA4CB01AD9E433C95A1A99,
+};
+static const int32_t s_InvokerIndices[41] = 
+{
+	1296,
+	1296,
+	1296,
+	1296,
+	1103,
+	671,
+	1296,
+	1296,
+	1296,
+	1296,
+	1296,
+	1296,
+	1296,
+	1296,
+	1103,
+	1103,
+	1296,
+	707,
+	1296,
+	1296,
+	824,
+	882,
+	1263,
+	1296,
+	1296,
+	1296,
+	1296,
+	1296,
+	521,
+	521,
+	1296,
+	1296,
+	1103,
+	1296,
+	1296,
+	1296,
+	1296,
+	1296,
+	1296,
+	1296,
+	1296,
+};
+extern const CustomAttributesCacheGenerator g_AssemblyU2DCSharp_AttributeGenerators[];
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	41,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	g_AssemblyU2DCSharp_AttributeGenerators,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
